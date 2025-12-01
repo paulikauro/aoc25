2c692b6512648d51537df006d50f10b7668a0562
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2025-12-01 19:36:22 +0200

    Solve day 1 part 2

--- day1.hs
+++ day1.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 
 import Data.List (scanl')
+import Control.Arrow ((&&&))
 
 parse :: String -> [Int]
 parse = map parseRot . lines
@@ -8,10 +9,25 @@ parse = map parseRot . lines
   parseRot (x:xs) = dir x * read xs
   dir = \case { 'L' -> -1; 'R' -> 1; }
 
-solve1 = show . count (== 0) . scanl' add 50
+solve1 = count (== 0) . scanl' add 50
   where
   count p = length . filter p
-  add a b = (a + b + 100) `mod` 100 -- vtf
 
-main = interact $ solve1 . parse
+add a b = (a + b) `mod` 100
+
+solve2trash = solve1 . (>>= repl)
+  where
+  repl x = replicate (abs x) (signum x)
+
+solve2 :: [Int] -> Int
+solve2 = fst . foldl' go (0, 50)
+  where
+  go (counts, acc) x = (counts + count' acc x, add acc x)
+  count' acc x = if x < 0 then count (mirror acc) (abs x) else count acc x
+  count acc x = wholes + if acc + rest >= 100 then 1 else 0
+    where
+    (wholes, rest) = x `divMod` 100
+  mirror = (`mod` 100) . negate
+
+main = interact $ show . (solve1 &&& solve2) . parse
 
