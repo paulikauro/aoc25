5ffbb2c8a6652af718b2d585a836e1332c37e4d4
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2025-12-02 12:50:50 +0200

    Solve day 2 part 1

---
+++ day2.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Data.List.Split (splitOn)
+import Data.List (singleton)
+
+
+parse :: String -> [(Int, Int)]
+parse = map (tup . map read . splitOn "-") . splitOn ","
+  where
+  tup [a, b] = (a, b)
+
+-- hax
+digits :: Int -> [Int]
+digits = map (read . singleton) . show
+
+solve1 = sum . filter isInvalid . (>>= uncurry enumFromTo)
+  where
+  isInvalid id = uncurry (==) $ splitAt (length d `div` 2) $ d
+    where
+    d = digits id
+
