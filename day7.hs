6c5e6dfeb9a0f775fff0c53270c308dd4a16d1bf
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2025-12-07 16:04:18 +0200

    Solve day 7 part 2

--- day7.hs
+++ day7.hs
@@ -1,13 +1,12 @@
-import Data.Array qualified as A
 import Data.Bits (shiftL, shiftR, (.|.), (.&.), popCount, complement)
+import Data.List (uncons)
+import Data.Maybe (fromJust)
 
-mkArray xs = A.listArray (0, length xs - 1) xs
-
-parse :: String -> (Integer, A.Array Int Integer)
-parse = (\(x:xs) -> (start x, rest xs)) . lines
+parse1 :: String -> (Integer, [Integer])
+parse1 = (\(x:xs) -> (start x, rest xs)) . lines
   where
   start = bittify 'S'
-  rest = mkArray . map (bittify '^')
+  rest = map (bittify '^')
   bittify c = foldl' step 0
     where
     step a x = shiftL a 1 .|. isC x
@@ -20,3 +19,14 @@ solve1 (start, lines) = fst $ foldl' step (0, start) lines
     hits = curr .&. line
     new = shiftL hits 1 .|. shiftR hits 1
     result = (curr .|. new) .&. complement hits
+
+parse2 :: String -> (String, [String])
+parse2 = fromJust . uncons . lines
+
+solve2 (start, lines) = sum $ foldl' step (map (fromEnum . (== 'S')) start) lines
+  where
+  step curr line = result
+    where
+    hits = zipWith (\a x -> if x == '^' then a else 0) curr line
+    new = zipWith (+) (tail hits ++ [0]) ([0] ++ hits)
+    result = zipWith (\x h -> if h > 0 then 0 else x) (zipWith (+) curr new) hits
