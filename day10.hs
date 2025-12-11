99f2962671b6f962a2aa55ef0ec6ca998d6b02b2
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2025-12-11 23:46:25 +0200

    Day 10 Gauss-Jordan elimination

--- day10.hs
+++ day10.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE BlockArguments, LambdaCase #-}
-import Data.Bits (shiftL, (.|.), xor, bit, popCount, testBit)
+import Data.Bits (shiftL, (.|.), (.&.), xor, bit, popCount, testBit)
 import Data.List.Split (splitOn)
-import Data.List (uncons, unsnoc, intercalate, sortOn)
-import Data.Foldable (toList)
+import Data.List (uncons, unsnoc, intercalate, sortOn, find, sortBy, foldl1')
+import Data.Foldable (toList, traverse_)
+import Data.Function (on)
 import Data.Array qualified as A
 import Data.Set qualified as Set
 import Data.Map.Strict qualified as Map
@@ -48,12 +49,17 @@ div' n = \case
   1 -> [[n]]
   k -> [0..n] >>= \i -> map (i:) $ div' (n - i) (k - 1)
 
+div'' n = \case
+  [] -> []
+  [k] -> if k >= n then [[n]] else []
+  k:ks -> [0..k] >>= \i -> map (i:) $ div'' (n - i) ks
+
 main = interact $ show . solve2 . parse
 solve2 = sum . map solve2machine'
 solve2machine' :: Machine -> Int
 solve2machine' ((_, btns, joltage) :: Machine) = traceShowId $ solve2machine (length btns) jolts
   where
-  jolts = sortOn (popCount . snd) $ map (second mask) $ zip joltage [0..]
+  jolts = sortOn fst {-(popCount . snd)-} $ map (second mask) $ zip joltage [0..]
   mask j = foldl' (\a b -> shiftL a 1 .|. fromEnum (j `elem` b)) (0 :: Int) $ reverse btns
 
 solve2machine nBtns jolts = minimum $ go (allZeros nBtns) jolts
@@ -72,10 +78,44 @@ solve2machine nBtns jolts = minimum $ go (allZeros nBtns) jolts
     if k == 0
       then go m js
       else do
-        d <- div' available k
+        let avs = availables available js m slots'
+        d <- if avs == [] then div' available k else div'' available avs
         go (m A.// zip slots' (map Just d)) js
   calcFixed bs m = sum [c | (i, Just c) <- A.assocs m, testBit bs i]
   slots bs m = [i | (i, Nothing) <- A.assocs m, testBit bs i]
   areOK js m = all (\(j, bs) -> calcFixed bs m <= j) js
+  availables a js m = map (\i -> minimum $ a : (map av' $ filter (hasI i) js))
+    where
+    av' (j, bs) = j - calcFixed bs m
+    hasI i (_, bs) = testBit bs i
   allZeros n = mkArray $ replicate n Nothing
+
+gj :: [[Int]] -> [[Int]]
+gj xs = descSort $ normRes $ foldl' step xs [0..length xs - 1]
+  where
+  step a ri = case pivot of
+    Nothing -> a
+    Just (pi, p) -> [if i == ri then row else elimRow er pi p row | (i, er) <- zip [0..] a]
+    where
+    row = a !! ri
+    pivot = find ((/= 0) . snd) $ zip [0..] row
+  elimRow er pi p pr = zipWith (+) (map (* erCoeff) er) $ map (* pCoeff) pr
+    where
+    erp = er !! pi
+    g = gcd erp p
+    pCoeff = negate (erp `div` g)
+    erCoeff = p `div` g
+  normRes = filter (any (/= 0)) . map normRow
+  normRow xs = map (\x -> (x `div` g) * signum' (last xs)) xs
+    where
+    g = if nonzero == [] then 1 else foldl1' gcd nonzero
+    nonzero = filter (/= 0) xs
+  -- 0 == signum 0 /= signum' 0 == 1
+  signum' x = if x < 0 then -1 else 1
+  descSort = sortBy (flip compare `on` map abs)
+
+toMatrix (_, btns, jolts) = zipWith (\j ji -> map (fromEnum . elem ji) (reverse btns) ++ [j]) jolts [0..]
+
+printArr :: [[Int]] -> IO ()
+printArr xs = traverse_ print xs >> putStrLn ""
 
